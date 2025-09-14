e03a08839b529da9746af3dc08de5717cb0076f0
CANDRA ALFIAN <54065360+viantmocy@users.noreply.github.com>
2025-09-14 22:03:43 +0700

    Update mining-boot.sh

--- ccminer/mining-boot.sh
+++ ccminer/mining-boot.sh
@@ -5,8 +5,11 @@ echo "[$(date)] Memulai mining loop..." >> $LOGFILE
 termux-wake-lock
 
 while true; do
-    echo "[$(date)] Menjalankan ccminer..." >> $LOGFILE
-    $HOME/verus-termux/ccminer/start.sh >> $LOGFILE 2>&1
+    echo "[$(date)] Menjalankan Verus Miner..." >> $LOGFILE
+    /data/data/com.termux/files/home/verus-termux/ccminer/start.sh >> $LOGFILE 2>&1
     echo "[$(date)] Miner crash/stop, restart dalam 10 detik..." >> $LOGFILE
     sleep 10
 done
+
+# Beri izin eksekusi
+chmod +x ~/.termux/boot/mining-boot.sh
