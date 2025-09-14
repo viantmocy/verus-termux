e28a45b1a97aa38fd69e5d1433d184507666d412
CANDRA ALFIAN <54065360+viantmocy@users.noreply.github.com>
2025-09-14 11:33:58 +0700

    Update termux-boot-setup.sh

--- termux-boot-setup.sh
+++ termux-boot-setup.sh
@@ -2,7 +2,7 @@
 BOOTDIR=$HOME/.termux/boot
 mkdir -p $BOOTDIR
 
-cp $HOME/verus-termux/mining-boot.sh $BOOTDIR/
+cp $HOME/verus-termux/ccminer/mining-boot.sh $BOOTDIR/
 chmod +x $BOOTDIR/mining-boot.sh
 
 echo "[*] Auto-boot sudah diset. Reboot HP untuk mulai mining otomatis."
